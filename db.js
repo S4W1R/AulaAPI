00576d0288fc7708b04e95f25ab58a369f0f9400
Samir carlos <samir_silva20@outlook.com>
2024-12-03 20:54:15 -0300

    Atualização

---
+++ db.js
@@ -0,0 +1,43 @@
+const Produto =[
+    {
+        id:1,
+        nome:"Iphone 8 Plus",
+        Quantidade:1,
+        Preço: 1000.00
+    },
+
+    {
+        id:1,
+        nome:"Iphone 14 Plus",
+        Quantidade:3,
+        Preço: 5215.00    }
+];
+
+
+function consultarProdutos(){
+    return clientes;
+}
+
+function consultarProduto(id){
+    return clientes.find(c => c.id == id);
+}
+
+function inserirProduto(cliente){
+    clientes.push(cliente);
+}
+
+function alteraProduto(id,){
+
+}
+function deletarProduto(cliente){
+    const cliente = clientes.findIndex(c => c.id == id);
+    cliente.splice(indice,1);
+}
+
+module.exports = {
+    consultarProdutos,
+    consultarProduto,
+    inserirProduto,
+    alteraProduto,
+    deletarProduto
+}
